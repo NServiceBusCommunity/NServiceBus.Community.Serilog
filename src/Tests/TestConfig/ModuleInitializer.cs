5a40e7c941d85063e391e8477f30df476cd7ec9c
Simon Cropp <simon.cropp@gmail.com>
2026-05-14 23:48:48 +1000

    .

--- src/Tests/TestConfig/ModuleInitializer.cs
+++ src/Tests/TestConfig/ModuleInitializer.cs
@@ -8,7 +8,12 @@
             var enrich = _.Enrich;
             enrich.WithExceptionDetails();
             enrich.WithNsbExceptionDetails();
-            _.Filter.ByExcluding(logEvent => logEvent.MessageTemplate.Text.StartsWith("Operation canceled while stopping"));
+            _.Filter.ByExcluding(logEvent =>
+            {
+                var text = logEvent.MessageTemplate.Text;
+                return text.StartsWith("Operation canceled while stopping") ||
+                       text.StartsWith("Started polling for new messages");
+            });
         });
         VerifierSettings.InitializePlugins();
         var nsbVersion = FileVersionInfo.GetVersionInfo(typeof(Endpoint).Assembly.Location);
@@ -32,9 +37,13 @@
             "NServiceBus.ExceptionInfo.Data.Handler start time :",
             "NServiceBus.ExceptionInfo.Data.Handler failure time :",
             "NServiceBus.ExceptionInfo.Data.Transport message ID :",
+            "$.diagnostics.hostid",
+            "$.diagnostics.originating.hostid",
             "traceparent :",
             "tunit.test.id",
-            "Completing processing for");
+            "Completing processing for",
+            "NServiceBus.Community.Serilog/",
+            "NServiceBus.Community.Serilog\\");
         VerifierSettings.AddExtraDateTimeOffsetFormat("yyyy-MM-ddTHH:mm:ss.fffzz");
 
         LogManager.Use<SerilogFactory>();

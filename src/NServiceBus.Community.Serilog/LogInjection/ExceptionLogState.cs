e4f1da6feb1c5b3d532b10072ae13230f6f2a8a8
Simon Cropp <simon.cropp@gmail.com>
2026-05-15 12:20:29 +1000

    Update ExceptionLogState.cs

--- src/NServiceBus.Community.Serilog/LogInjection/ExceptionLogState.cs
+++ src/NServiceBus.Community.Serilog/LogInjection/ExceptionLogState.cs
@@ -1,6 +1,5 @@
 ﻿#pragma warning disable 1591
 
-[Serializable]
 class ExceptionLogState(LogEventProperty processingEndpoint, IReadOnlyDictionary<string, string> incomingHeaders, string? correlationId, string? conversationId)
 {
     public readonly LogEventProperty ProcessingEndpoint = processingEndpoint;

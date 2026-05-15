a8082e301530adf904cb477e88d0f7275b9dd416
Simon Cropp <simon.cropp@gmail.com>
2026-05-15 12:23:44 +1000

    throw for no ILogger in context

--- src/NServiceBus.Community.Serilog/LogInjection/MessageContextBehavior.cs
+++ src/NServiceBus.Community.Serilog/LogInjection/MessageContextBehavior.cs
@@ -14,8 +14,12 @@
 
     public override Task Invoke(IIncomingLogicalMessageContext context, Func<Task> next)
     {
-        var bag = context.Extensions;
-        var state = bag.Get<ExceptionLogState>();
+        if (!context.Extensions.TryGet<ExceptionLogState>(out var state))
+        {
+            throw new InvalidOperationException(
+                $"Expected an {nameof(ExceptionLogState)} in the pipeline context. Ensure {nameof(IncomingPhysicalBehavior)} is registered before {nameof(MessageContextBehavior)}.");
+        }
+
         state.IncomingMessage = context.Message.Instance;
         return next();
     }

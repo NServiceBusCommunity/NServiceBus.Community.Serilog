9032134415abcabf3d0bc480eaa74470f6d4e9d4
Simon Cropp <simon.cropp@gmail.com>
2026-05-15 12:20:34 +1000

    Update LoggerFactory.cs

--- src/NServiceBus.Community.Serilog/LoggerFactory.cs
+++ src/NServiceBus.Community.Serilog/LoggerFactory.cs
@@ -4,12 +4,12 @@
     public ILog GetLogger(Type type)
     {
         var contextLogger = logger.ForContext(type);
-        return new Logger(contextLogger);
+        return new NServiceBus.Serilog.Logger(contextLogger);
     }
 
     public ILog GetLogger(string name)
     {
         var contextLogger = logger.ForContext("SourceContext", name);
-        return new Logger(contextLogger);
+        return new NServiceBus.Serilog.Logger(contextLogger);
     }
 }

dec243245a5adbe36742b9d67a3ecdd618378bfb
Simon Cropp <simon.cropp@gmail.com>
2026-05-15 12:16:29 +1000

    Throw if frozen in HeaderAppender (#821)

---
+++ src/Tests/HeaderAppenderTests.cs
@@ -0,0 +1,59 @@
+using NServiceBus.Serilog;
+
+[NotInParallel("HeaderAppender.frozen")]
+public class HeaderAppenderTests
+{
+    [Test]
+    public async Task ExcludeAddsHeadersBeforeFreeze()
+    {
+        HeaderAppender.ResetForTests();
+        try
+        {
+            #region ExcludeHeaders
+            HeaderAppender.Exclude("MyCustomHeader");
+            HeaderAppender.Exclude("HeaderA", "HeaderB", "HeaderC");
+            #endregion
+
+            var headers = new Dictionary<string, string>
+            {
+                ["MyCustomHeader"] = "0",
+                ["HeaderA"] = "1",
+                ["HeaderB"] = "2",
+                ["HeaderC"] = "3",
+                ["HeaderD"] = "4"
+            };
+            var properties = HeaderAppender.BuildHeaders(headers, convertHeader: null).ToList();
+
+            await Assert.That(properties).HasSingleItem();
+            var other = (DictionaryValue) properties[0].Value;
+            await Assert.That(other.Elements.Keys.Select(_ => (string) _.Value!)).IsEquivalentTo(["HeaderD"]);
+        }
+        finally
+        {
+            HeaderAppender.ResetForTests();
+            HeaderAppender.Freeze();
+        }
+    }
+
+    [Test]
+    public async Task ExcludeParamsThrowsAfterFreeze()
+    {
+        HeaderAppender.ResetForTests();
+        HeaderAppender.Freeze();
+
+        Assert.Throws<InvalidOperationException>(() => HeaderAppender.Exclude("A", "B"));
+
+        await Task.CompletedTask;
+    }
+
+    [Test]
+    public async Task ExcludeThrowsAfterFreeze()
+    {
+        HeaderAppender.ResetForTests();
+        HeaderAppender.Freeze();
+
+        var exception = Assert.Throws<InvalidOperationException>(() => HeaderAppender.Exclude("TooLate"));
+
+        await Assert.That(exception!.Message).Contains("frozen");
+    }
+}
